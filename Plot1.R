7a19eacdaaaab4d31a9c3e5a09a98f10d79e5e26
jhon <67615289+jhon1217@users.noreply.github.com>
2020-08-30 19:23:40 -0500

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,6 +1,6 @@
 library("data.table")
 
-setwd("~/Documents/Coursera/Exploratory Data Analysis/")
+setwd("~/Documents/Coursera/Exploratory Data Analysis")
 
 #Reads in data from file then subsets data for specified dates
 powerDT <- data.table::fread(input = "household_power_consumption.txt"
